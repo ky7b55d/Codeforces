b7085f4de75493d8dab12d6fd664fd3e606f5de6
kyk1os <kyzhle0613@gmail.com>
2026-04-01 00:45:21 +0800

    default

---
+++ practice/1067A.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+using ll = long long;
+
+void solve() {
+    int n; cin >> n;
+    int y,r; cin >> y >> r;
+
+    int ans=min(n,r+y/2);
+    cout << ans << "\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T;
+    cin >> T;
+    while (T--) solve();
+    return 0;
+}
