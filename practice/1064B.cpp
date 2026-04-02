23075e98ac31caa689fb8f8d179531b3a752bf83
kyk1os <kyzhle0613@gmail.com>
2026-04-02 21:33:17 +0800

    default

---
+++ practice/1064B.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+using ll = long long;
+
+void solve() {
+    ll a,b,n; cin >> a >> b >> n;
+
+    if (n*b <= a || a==b) cout << 1 << "\n";
+    else cout << 2 << "\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T;
+    cin >> T;
+    while (T--) solve();
+    return 0;
+}
