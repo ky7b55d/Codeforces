97dee6a7d993450a7a135c2712e214ee04a301d5
kyk1os <kyzhle0613@gmail.com>
2026-04-05 23:09:20 +0800

    default

--- practice/edu184A.cpp
+++ practice/edu184A.cpp
@@ -15,7 +15,15 @@ using namespace std;
 using ll = long long;
 
 void solve() {
-    
+    int n,a; cin >> n >> a;
+    vector<int> arr(n);
+    for (int i=0;i<n;i++) cin >> arr[i];
+
+    int lower=lower_bound(arr.begin(),arr.end(),a)-arr.begin();
+    int greater=arr.end()-upper_bound(arr.begin(),arr.end(),a);
+
+    if (lower>=greater) cout << a-1 << "\n";
+    else cout << a+1 << "\n";
 }
 
 int main() {
