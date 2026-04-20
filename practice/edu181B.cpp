35f1fcfe373a63629ab6b10c5717f578813a8f79
kyk1os <kyzhle0613@gmail.com>
2026-04-20 23:32:18 +0800

    default

---
+++ practice/edu181B.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+using ll = long long;
+
+void solve() {
+     ll a,b,k; cin >> a >> b >> k;
+     
+     ll x=gcd(a,b);
+    if (a/x<=k && b/x<=k) cout << 1 << "\n";
+    else cout << 2 << "\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T;
+    cin >> T;
+    while (T--) solve();
+    return 0;
+}
