3e90f71f135777075d685e8269cbb35b0a8d497d
kyk1os <kyzhle0613@gmail.com>
2026-04-20 22:54:48 +0800

    default

---
+++ practice/edu181A.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+using ll = long long;
+
+void solve() {
+    string s; cin >> s;
+    sort(s.rbegin(),s.rend());
+    cout << s << "\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T;
+    cin >> T;
+    while (T--) solve();
+    return 0;
+}
