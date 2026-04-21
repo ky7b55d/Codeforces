b0d08fbf9de23ae2d550774881b62529e79f99b0
kyk1os <kyzhle0613@gmail.com>
2026-04-21 12:15:30 +0800

    default

---
+++ practice/1057A.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+using ll = long long;
+
+void solve() {
+    int n; cin >> n;
+    vector<int> b(n);
+    for (int i=0;i<n;i++) cin >> b[i];
+
+    set<int> st(b.begin(),b.end());
+    cout << st.size() << "\n";
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T;
+    cin >> T;
+    while (T--) solve();
+    return 0;
+}
