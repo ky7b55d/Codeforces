1234949b6cf59a59d50e423fe044159d4fa44641
kyk1os <kyzhle0613@gmail.com>
2026-04-24 10:58:09 +0800

    default

---
+++ practice/1056A.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+using ll = long long;
+
+void solve() {
+    int n; cin >> n;
+    cout << 2*n-2 << "\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T;
+    cin >> T;
+    while (T--) solve();
+    return 0;
+}
