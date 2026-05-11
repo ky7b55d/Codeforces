da90913da655fd092ef5173c7352c4ddc523b801
kyk1os <kyzhle0613@gmail.com>
2026-05-11 18:20:05 +0800

    default

---
+++ practice/edu189A.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+using ll = long long;
+
+void solve() {
+    ll x,y; cin >> x >> y;
+    if(y>2*x) cout << "YES\n";
+    else cout << "NO\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T;
+    cin >> T;
+    while (T--) solve();
+    return 0;
+}
