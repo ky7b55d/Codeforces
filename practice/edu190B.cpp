2a220219821c6c9c61708b8b94bcff2e567e5d5c
kyk1os <kyzhle0613@gmail.com>
2026-05-20 10:42:57 +0800

    default

--- practice/edu190B.cpp
+++ practice/edu190B.cpp
@@ -16,31 +16,20 @@ using ll = long long;
 
 void solve() {
     string s; cin >> s;
-    int ans=count(s.begin(),s.end(),'4');
-    s.erase(remove(s.begin(), s.end(), '4'), s.end());
-
-    while (true){
-        string t;
-        int add=0;
-        for (int i=0;i<s.size();){
-            if(i+1<s.size() && 
-            ((s[i]=='1' && s[i+1]=='2') ||
-             (s[i]=='3' && s[i+1]=='2'))){
-                add++;
-                i+=2;
-            }
-            else{
-                t.push_back(s[i]);
-                i++;
-            }
-        }
-        if (add==0) break;
-        ans+=add;
-        s=t;
+    
+    int right=0;
+    for (char c:s){
+        if (c=='1'||c=='3') right++;
     }
-    
-    cout << ans << "\n";
-    
+    int left=0;
+    int best=right;
+
+    for (char c:s){
+        if (c=='1'||c=='3') right--;
+        if (c=='2') left++;
+        best=max(best,left+right);
+    }
+    cout << s.size()-best << "\n";
 }
 
 int main() {
