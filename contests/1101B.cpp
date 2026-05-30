6da1b99f77d1cb8d40d137cc97cc45ef281ab913
kyk1os <kyzhle0613@gmail.com>
2026-05-30 22:40:20 +0800

    default

---
+++ contests/1101B.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+using ll = long long;
+
+void solve() {
+    int n;
+    cin >> n;
+
+    ll sum = 0;
+    ll ans = (ll)4e18;
+
+    for (int i = 1; i <= n; i++) {
+        ll x;
+        cin >> x;
+        sum += x;
+        ans = min(ans, sum / i);
+        cout << ans << " ";
+    }
+    cout << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T;
+    cin >> T;
+    while (T--) solve();
+    return 0;
+}
